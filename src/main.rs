cd1d2921e94598fea9da0ce6ba9d5757f9892b6d
Pedro Santayana <pedrosantayana@gmail.com>
2024-08-22 16:06:16 -0300

    some explorations

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-use cranelift::{codegen::ir::{self, Function}, prelude::{isa::CallConv, AbiParam, FunctionBuilder, FunctionBuilderContext, Signature}};
-use pest_derive::Parser;
+use cranelift::{
+    codegen::ir::{self, Function},
+    prelude::{
+        isa::CallConv, AbiParam, EntityRef, FunctionBuilder, FunctionBuilderContext, InstBuilder, Signature, Value, Variable
+    },
+};
 use pest::Parser;
+use pest_derive::Parser;
 
 #[derive(Parser)]
 #[grammar = "grammar.pest"]
@@ -9,12 +14,10 @@ struct MyParser;
 fn main() {
     println!("Hello, world!");
 
-
     let mut sig = Signature::new(CallConv::SystemV);
 
     sig.returns.push(AbiParam::new(ir::types::I32));
     sig.params.push(AbiParam::new(ir::types::I32));
-
 
     let mut fn_builder_ctx = FunctionBuilderContext::new();
 
@@ -22,32 +25,51 @@ fn main() {
 
     let mut builder = FunctionBuilder::new(&mut func, &mut fn_builder_ctx);
 
+    let block0 = builder.create_block();
+
+    
+
     let parser = MyParser::parse(
-        Rule::program, 
+        Rule::program,
         r"
         var num = 10
         num = 12
         print(num)
-        "
-    ).unwrap();
+        ",
+    )
+    .unwrap();
 
     for pair in parser {
-        println!("Rule:    {:?}", pair.as_rule());
-        println!("Span:    {:?}", pair.as_span());
-        println!("Text:    {}", pair.as_str());
+        // println!("Rule:    {:?}", pair.as_rule());
+        // println!("Span:    {:?}", pair.as_span());
+        // println!("Text:    {}", pair.as_str());
 
         match pair.as_rule() {
-            Rule::var_declaration => todo!(),
-            Rule::fn_call => todo!(),
-            Rule::var_assignment => todo!(),
-            Rule::EOI => todo!(),
-            Rule::program => todo!(),
-            Rule::statement => todo!(),
-            Rule::identifier => todo!(),
-            Rule::number => todo!(),
-            Rule::literal => todo!(),
-            Rule::expression => todo!(),
-            Rule::WHITESPACE => todo!(),
+            Rule::var_declaration => {
+                let mut pairs = pair.into_inner();
+
+                let identifier = pairs.next().unwrap();
+                let value = pairs.next().unwrap().into_inner().next().unwrap();
+                println!("{} {}", identifier.as_str(), value.as_str());
+
+                let val_u32 = value.as_str().parse::<i64>().unwrap();
+
+                let tmp = builder.ins().iconst(ir::types::I32, val_u32);
+
+                let var = Variable::new(0);
+                builder.declare_var(var, ir::types::I32);
+                builder.def_var(var, tmp);
+            }
+            Rule::fn_call => {}
+            Rule::var_assignment => {}
+            Rule::EOI => {}
+            Rule::program => {}
+            Rule::statement => {}
+            Rule::identifier => {}
+            Rule::number => {}
+            Rule::literal => {}
+            Rule::expression => {}
+            Rule::WHITESPACE => {}
         }
     }
 }
