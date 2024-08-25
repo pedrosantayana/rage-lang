75e2447c0bf3a59454b3c16c73eb9bb6a14ae011
Pedro Santayana <pedrosantayana@gmail.com>
2024-08-24 21:09:59 -0300

    refactor: first refactoring

---
+++ src/compiler.rs
@@ -0,0 +1,103 @@
+use std::{collections::HashMap, fs::File};
+
+use cranelift::{
+    codegen::{ir::{self, FuncRef, Function, UserFuncName}, Context},
+    prelude::{
+        isa, settings, AbiParam, Configurable, FunctionBuilder, FunctionBuilderContext, InstBuilder, Signature
+    },
+};
+use cranelift_module::{Linkage, Module};
+use cranelift_object::ObjectModule;
+
+use crate::parser;
+
+pub fn compile(input: &str) {
+    let mut shared_builder = settings::builder();
+    shared_builder.enable("is_pic").unwrap();
+    let shared_flags = settings::Flags::new(shared_builder);
+
+    // ISA and Call Convetion
+    let isa_builder = isa::lookup(target_lexicon::triple!("x86_64-linux-gnu")).unwrap();
+    let isa = isa_builder.finish(shared_flags).unwrap();
+    let call_conv = isa.default_call_conv();
+
+    // Signature
+    let mut sig = Signature::new(call_conv);
+    sig.returns.push(AbiParam::new(ir::types::I32));
+
+    // FuncId declaration
+    let obj_builder = cranelift_object::ObjectBuilder::new(
+        isa,
+        "main",
+        cranelift_module::default_libcall_names(),
+    )
+    .unwrap();
+    let mut obj_module = cranelift_object::ObjectModule::new(obj_builder);
+
+    let fid = obj_module
+        .declare_function("main", Linkage::Export, &sig)
+        .unwrap();
+
+    // FunctionBuilder declaration (triplice aliança do capeta das mut ref, NUNCA refatorar!) TODO: Estudar mais sobre lifetimes
+    let mut cl_fn = Function::with_name_signature(UserFuncName::user(0, 0), sig);
+    let mut cl_fn_builder_ctx = FunctionBuilderContext::new();
+    let cl_fn_builder = FunctionBuilder::new(&mut cl_fn, &mut cl_fn_builder_ctx);
+
+    // Libc definitions
+    //let mut libc_decl;
+    let (mut obj_module, mut cl_fn_builder, libc_decl) = gen_libc(obj_module, cl_fn_builder);
+
+   
+
+
+    // Main block declaration
+    let block = cl_fn_builder.create_block();
+    cl_fn_builder.switch_to_block(block);
+    cl_fn_builder.seal_block(block);
+
+    // Parser
+    cl_fn_builder = parser::PedroParser::parse(input, cl_fn_builder, libc_decl);
+
+    // Return 0 and seal all blocks
+    let val = cl_fn_builder.ins().iconst(ir::types::I32, 0);
+    cl_fn_builder.ins().return_(&[val]);
+    cl_fn_builder.seal_all_blocks();
+    cl_fn_builder.finalize();
+
+    // Object generation, using cranelift-object crate
+    let mut context = Context::for_function(cl_fn);
+    obj_module.define_function(fid, &mut context).unwrap();
+    let res = obj_module.finish();
+
+    // Write object bytes to file
+    let mut file = File::create("OUTPUT.o").unwrap();
+    res.object.write_stream(&mut file).unwrap();
+}
+
+fn gen_libc<'a>(obj_module: ObjectModule, cl_fn_builder: FunctionBuilder<'a>) -> (ObjectModule, FunctionBuilder<'a>, HashMap<PedroLibC, FuncRef>) {
+    let mut libc_decl: HashMap<PedroLibC, FuncRef> = HashMap::new();
+
+    let (obj_module, cl_fn_builder,  f) = libc_putchar_decl(obj_module, cl_fn_builder);
+
+    libc_decl.insert(PedroLibC::PutChar, f);
+
+    (obj_module, cl_fn_builder, libc_decl)
+}
+
+fn libc_putchar_decl<'a>(mut obj_module: ObjectModule, cl_fn_builder: FunctionBuilder<'a>) -> (ObjectModule, FunctionBuilder<'a>, FuncRef) {
+    let mut putchar_sig = obj_module.make_signature();
+    putchar_sig.params.push(AbiParam::new(ir::types::I8));
+    putchar_sig.returns.push(AbiParam::new(ir::types::I32));
+    let putchar_fn = obj_module
+        .declare_function("putchar", Linkage::Import, &putchar_sig)
+        .unwrap();
+    let local_putchar = obj_module.declare_func_in_func(putchar_fn, cl_fn_builder.func);
+
+    (obj_module, cl_fn_builder, local_putchar)
+}
+
+#[derive(Eq, Hash, PartialEq)]
+pub enum PedroLibC {
+    PutChar,
+    GetChar
+}
---
+++ src/parser.rs
@@ -0,0 +1,181 @@
+
+use std::collections::HashMap;
+
+use cranelift::{
+    codegen::ir::{self, FuncRef},
+    prelude::{FunctionBuilder, InstBuilder, Variable},
+};
+use pest::{
+    iterators::Pair,
+    Parser,
+};
+use pest_derive::Parser;
+
+use crate::compiler::PedroLibC;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+struct PedroPestParser;
+
+#[derive(Debug)]
+struct PedroVariable {
+    pub cl_variable: Variable,
+    pub cl_type: ir::types::Type,
+    //pub cl_index: usize,
+    //pub identifier: String,
+}
+
+
+pub struct PedroParser<'a> {
+    cl_fn_builder: FunctionBuilder<'a>,
+    variables: HashMap<String, PedroVariable>,
+    pedro_libc: HashMap<PedroLibC, FuncRef>
+}
+
+impl PedroParser<'_> {
+    pub fn parse<'a>(input: &'a str, cl_fn_builder: FunctionBuilder<'a>, pedro_libc: HashMap<PedroLibC, FuncRef>) -> FunctionBuilder<'a> {
+        let pairs = PedroPestParser::parse(Rule::program, input).unwrap();
+
+        let mut pedro_parser = PedroParser {
+            cl_fn_builder,
+            variables: HashMap::new(),
+            pedro_libc
+        };
+
+        for pair in pairs {
+            match pair.as_rule() {
+                Rule::var_declaration => pedro_parser.parse_var_decl(pair),
+                Rule::var_assignment => pedro_parser.parse_var_assign(pair),
+                Rule::fn_call => pedro_parser.parse_fn_call(pair),
+                Rule::EOI => {}
+                Rule::program => {}
+                Rule::statement => {}
+                Rule::identifier => {}
+                Rule::number => {}
+                Rule::literal => {}
+                Rule::expression => {}
+                Rule::WHITESPACE => {}
+                Rule::primitive_types => {}
+            }
+        }
+
+        pedro_parser.cl_fn_builder
+    }
+
+    fn parse_var_decl(&mut self, pair: Pair<Rule>) {
+        let mut pairs = pair.into_inner();
+
+        let identifier = pairs.next().unwrap().as_str();
+        let pedro_type_str = pairs.next().unwrap().as_str();
+
+        let pedro_type = PedroTypes::from_str(pedro_type_str).unwrap();
+
+        let index = self.variables.len();
+        let var = Variable::from_u32(index.try_into().unwrap());
+
+        self.cl_fn_builder.declare_var(var, pedro_type.to_cl_type());
+
+        let pedro_var = PedroVariable {
+            cl_variable: var,
+            cl_type: pedro_type.to_cl_type(),
+            //cl_index: index,
+            //identifier: identifier.to_owned(),
+        };
+
+        self.variables.insert(identifier.to_string(), pedro_var);
+    }
+
+    fn parse_var_assign(&mut self, pair: Pair<Rule>) {
+        let mut pairs = pair.into_inner();
+
+        let identifier = pairs.next().unwrap().as_str();
+        let value_str = pairs.next().unwrap().into_inner().next().unwrap().as_str();
+
+        println!("{} {}", identifier, value_str);
+
+        let pedro_var = self.variables.get(identifier).unwrap();
+
+        let parsed_value;
+
+        match pedro_var.cl_type {
+            ir::types::I8 => parsed_value = value_str.parse::<i64>().unwrap(),
+            ir::types::I32 | _ => parsed_value = value_str.parse::<i64>().unwrap(),
+        }
+
+        let cl_val = self
+            .cl_fn_builder
+            .ins()
+            .iconst(pedro_var.cl_type, parsed_value);
+
+            self
+            .cl_fn_builder
+            .def_var(pedro_var.cl_variable, cl_val);
+    }
+
+    fn parse_fn_call(&mut self, pair: Pair<Rule>) {
+        let mut pairs = pair.into_inner();
+
+        let fn_name = pairs.next().unwrap().as_str();
+        //let expr_str = pairs.next().unwrap().into_inner().next().unwrap().as_str();
+
+        println!("{}", fn_name);
+
+        if fn_name.eq("libc_putchar") {
+            for p in pairs.next().unwrap().into_inner() {
+                match p.as_rule() {
+                    Rule::number => {}
+                    Rule::identifier => {
+                        let pedro_var = self.variables.get(p.as_str()).unwrap();
+
+                        let cl_val =
+                        self.cl_fn_builder.use_var(pedro_var.cl_variable);
+                        self.cl_fn_builder.ins().call(*self.pedro_libc.get(&PedroLibC::PutChar).unwrap(), &[cl_val]);
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+}
+
+pub enum PedroTypes {
+    I8,
+    I16,
+    I32,
+    I64,
+    // U8,
+    // U16,
+    // U32,
+    // U64,
+    F32,
+    F64,
+    // Char,
+    Boolean,
+}
+
+impl PedroTypes {
+    pub fn from_str(input: &str) -> Option<PedroTypes> {
+        match input {
+            "i8" => Some(PedroTypes::I8),
+            "i16" => Some(PedroTypes::I16),
+            "i32" => Some(PedroTypes::I32),
+            "i64" => Some(PedroTypes::I64),
+            "f32" => Some(PedroTypes::F32),
+            "f64" => Some(PedroTypes::F64),
+            "bool" => Some(PedroTypes::Boolean),
+            _ => None,
+        }
+    }
+
+    pub fn to_cl_type(&self) -> ir::types::Type {
+        match self {
+            PedroTypes::I8 => ir::types::I8,
+            PedroTypes::I16 => ir::types::I16,
+            PedroTypes::I32 => ir::types::I32,
+            PedroTypes::I64 => ir::types::I64,
+            PedroTypes::F32 => ir::types::F32,
+            PedroTypes::F64 => ir::types::F64,
+            PedroTypes::Boolean => ir::types::I8,
+        }
+    }
+}
